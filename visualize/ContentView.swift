ebdbedc1ed9e82dacc6ebade69ca163110dc61a1
Crlss123 <camadortjr@gmail.com>
2026-04-11 14:05:33 -0700

    Initial Commit

---
+++ visualize/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  visualize
+//
+//  Created by Carlos Amador on 11/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ visualize/visualizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  visualizeApp.swift
+//  visualize
+//
+//  Created by Carlos Amador on 11/04/26.
+//
+
+import SwiftUI
+
+@main
+struct visualizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
