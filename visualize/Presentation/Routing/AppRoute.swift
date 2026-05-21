4bb8d80e75f0e84b0b444cc14f9db6789ad843cf
Miguel Degollado <A01255388@tec.mx>
2026-05-20 19:43:20 -0700

    Add Notification case to App Route

--- visualize/Presentation/Routing/AppRoute.swift
+++ visualize/Presentation/Routing/AppRoute.swift
@@ -5,13 +5,6 @@
 //  Created by Libia Fv on 10/05/26.
 //
 
-// MARK: - App Route
-
-/// Defines all possible navigation destinations
-/// within the main navigation stack.
-///
-/// Used by `AppCoordinator` to push and replace
-/// screens via SwiftUI's `NavigationStack`.
 enum AppRoute: Hashable {
     case login
     case signUp
@@ -19,15 +12,9 @@ enum AppRoute: Hashable {
     case checkEmail(email: String)
     case generatingVisualizations
     case vizReady
+    case notifications
 }
 
-// MARK: - Root Route
-
-/// Defines the root-level screen of the app,
-/// displayed before any navigation occurs.
-///
-/// Separate from `AppRoute` since it lives outside
-/// the `NavigationStack` path.
 enum RootRoute {
     case landing
 }
