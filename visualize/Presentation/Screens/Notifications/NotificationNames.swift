b733f44a91ad0e2a3b8b8d1ae289d1dfcbdf42a3
Miguel Degollado <A01255388@tec.mx>
2026-05-20 20:23:17 -0700

    Add notification screen lifecycle notification names

---
+++ visualize/Presentation/Screens/Notifications/NotificationNames.swift
@@ -0,0 +1,14 @@
+//
+//  NotificationNames.swift
+//  visualize
+//
+//  Created by Miguel Degollado on 20/05/26.
+//
+
+import Foundation
+
+extension Foundation.Notification.Name {
+    static let notificationsScreenDidAppear    = Foundation.Notification.Name("notificationsScreenDidAppear")
+    static let notificationsScreenDidDisappear = Foundation.Notification.Name("notificationsScreenDidDisappear")
+}
+
