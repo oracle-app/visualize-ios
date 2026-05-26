d104ad0e16ae4379db0bbcdbf9e338b71419a023
Miguel Degollado <A01255388@tec.mx>
2026-05-26 11:44:30 -0700

    fix(notifications): update GetNotificationsUseCase to return AsyncStream of domain Notification entities

--- visualize/Domain/UseCases/GetNotificationsUseCase.swift
+++ visualize/Domain/UseCases/GetNotificationsUseCase.swift
@@ -2,15 +2,15 @@
 //  GetNotificationsUseCase.swift
 //  visualize
 //
-//  Created by SOPORTE on 20/05/26.
+// created by Miguel Degollado
 
 import Foundation
 
 final class GetNotificationsUseCase {
     private let repository: NotificationRepository
     init(repository: NotificationRepository) { self.repository = repository }
-    func execute(for userID: String) -> AsyncStream<[NotificationDisplayGroup]> {
+
+    func execute(for userID: String) -> AsyncStream<[Notification]> {
         repository.notificationsStream(for: userID)
     }
 }
-
